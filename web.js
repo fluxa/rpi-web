54a6d26ca3bb0782425dda9b88e43a2d61e8414e
fluxa <jmfluxa@gmail.com>
2012-08-08 23:32:05 +0800

    first

---
+++ web.js
@@ -0,0 +1,27 @@
+//Imports
+var express = require('express');
+var app = express.createServer(express.logger());
+var fs = require('fs');
+
+var response_template = "{\"success\":\"$SUCCESS\",\"error\":\"$ERROR\",\"data\":$DATA}";
+
+// Configuration
+app.configure(function(){
+  //app.set('views', __dirname + '/views');
+  //app.set('view engine', 'jade');
+  //app.set('view options', { layout: false });
+  app.use(express.bodyParser());
+  //app.use(express.methodOverride());
+  //app.use(express.cookieParser());
+  //app.use(express.session({ secret: 'your secret here' }));
+  //app.use(app.router);
+  //app.use(express.static(__dirname + '/public'));
+});
+
+
+//Wiring
+
+app.get('/', function(req, res) {
+  //res.render('index', robj);
+  res.send('I\m running from a raspberry-pi. Hello there!');
+});
